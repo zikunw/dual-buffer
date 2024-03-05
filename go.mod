27861985a6eaf3b9f02a23ccf69badef901aff17
Zikun Wang <zikunw@bu.edu>
2024-03-05 14:54:34 -0500

    init

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/zikunw/dual-buffer
+
+go 1.21.6
---
+++ main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"log"
+	"time"
+)
+
+func main() {
+	db := NewDualBuffer(func(b *buffer) error {
+		log.Println("Processing buffer", b)
+		time.Sleep(time.Duration(500) * time.Millisecond)
+		return nil
+	})
+	kv := KV{}
+	for i := 0; i < 1024; i++ {
+		kv.Key = "key"
+		kv.Value = []byte("value")
+		db.Write(&kv)
+		log.Println("Writing", i)
+		time.Sleep(time.Duration(50) * time.Millisecond)
+	}
+}
